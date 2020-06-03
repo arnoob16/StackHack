ed2f9a7295b38b9e6dd8ff97d86e2b0c7363920a
Subhradeep Nag <52783389+subhradeepnag@users.noreply.github.com>
2020-06-03 20:18:21 +0530

    login only

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
+const bcrypt = require("bcrypt");
 
 
 const app = express();
 
+const users = [];
+
+
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view-engine','ejs');
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login",function(req,res){
+    res.sendFile(__dirname + "/login.html");
+});
+app.get("/signup",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/",function(req,res){
@@ -24,6 +38,34 @@ app.post("/",function(req,res){
             }
         ]
     };
+
+
+app.post("/signup", async(req,res) => {
+
+        try{
+            const hashedPassword = await bcrypt.hash(req.body.password,10)
+            users.push({
+                id: Date.now().toString(),
+                name: req.body.name,
+                email: req.body.email,
+                password : req.body.password,
+                confirm_password : hashedPassword
+            })
+            res.redirect("/login");
+        }catch{
+            res.redirect("/signup");
+        }
+        console.log(users);
+        
+    });
+        
+    
+app.post("/login",function(req,res){
+
+});
+
+
+ 
     const jsonData = JSON.stringify(data);
     const url = "https://us19.api.mailchimp.com/3.0/lists/a86b6611d6"
     const options ={
