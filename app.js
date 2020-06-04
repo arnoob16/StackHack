da9e94b7e19fb4e53db96c2965f2019cb0f1dc29
Subhradeep Nag <52783389+subhradeepnag@users.noreply.github.com>
2020-06-04 23:58:45 +0530

    added backend

--- app.js
+++ app.js
@@ -1,30 +1,81 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require('method-override')
+
 
 
 const app = express();
 
+const intializePassport = require('./passport-config');
+intializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+     id => users.find(user => user.id === id)   
+    )
+
+
 const users = [];
+
+app.set('view engine', 'ejs');
 
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(flash());
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
-app.set('view-engine','ejs');
-
-app.get("/",function(req,res){
-    res.sendFile(__dirname + "/index.html");
+app.get("/",checkAuthenticated,function(req,res){
+    res.render("index.ejs");
 });
 
-app.get("/login",function(req,res){
-    res.sendFile(__dirname + "/login.html");
+app.get("/login",checkNotAuthenticated,(req,res)=>{
+    res.render("login.ejs");
 });
-app.get("/signup",function(req,res){
-    res.sendFile(__dirname + "/signup.html");
+
+app.post('/login', checkNotAuthenticated,passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+app.get("/signup",checkNotAuthenticated,(req,res)=>{
+    res.render("signup.ejs");
+}); 
+app.post("/signup", checkNotAuthenticated,async(req,res) => {
+
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password : hashedPassword,
+            confirm_password : hashedPassword
+        })
+        res.redirect("/login");
+    }catch{
+        res.redirect("/signup");
+    }
+    console.log(users);
+    
 });
 
 app.post("/",function(req,res){
@@ -40,32 +91,6 @@ app.post("/",function(req,res){
     };
 
 
-app.post("/signup", async(req,res) => {
-
-        try{
-            const hashedPassword = await bcrypt.hash(req.body.password,10)
-            users.push({
-                id: Date.now().toString(),
-                name: req.body.name,
-                email: req.body.email,
-                password : req.body.password,
-                confirm_password : hashedPassword
-            })
-            res.redirect("/login");
-        }catch{
-            res.redirect("/signup");
-        }
-        console.log(users);
-        
-    });
-        
-    
-app.post("/login",function(req,res){
-
-});
-
-
- 
     const jsonData = JSON.stringify(data);
     const url = "https://us19.api.mailchimp.com/3.0/lists/a86b6611d6"
     const options ={
@@ -90,6 +115,26 @@ app.post("/login",function(req,res){
     request.end();
 
 });
+
+app.delete('/logout',(req,res)=>{
+    req.logOut()
+    res.redirect('/login');
+})
+
+function checkAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    res.redirect('/login');
+}
+
+function checkNotAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+       return res.redirect('/')
+    }
+    next();
+}
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
