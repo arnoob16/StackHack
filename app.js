90a2f8e5a53b790d9f1cfc29ec0287d75dac409e
Subhradeep Nag <52783389+subhradeepnag@users.noreply.github.com>
2020-06-05 09:45:24 +0530

    Proper Backend

--- app.js
+++ app.js
@@ -43,23 +43,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
 
-app.get("/",checkAuthenticated,function(req,res){
+app.get("/",function(req,res){
     res.render("index.ejs");
 });
 
-app.get("/login",checkNotAuthenticated,(req,res)=>{
+app.get("/login",(req,res)=>{
     res.render("login.ejs");
 });
 
-app.post('/login', checkNotAuthenticated,passport.authenticate('local', {
+app.post('/login',passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
   }));
-app.get("/signup",checkNotAuthenticated,(req,res)=>{
+app.get("/signup",(req,res)=>{
     res.render("signup.ejs");
 }); 
-app.post("/signup", checkNotAuthenticated,async(req,res) => {
+app.post("/signup",async(req,res) => {
 
     try{
         const hashedPassword = await bcrypt.hash(req.body.password,10)
@@ -121,20 +121,20 @@ app.delete('/logout',(req,res)=>{
     res.redirect('/login');
 })
 
-function checkAuthenticated(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
+// function checkAuthenticated(req,res,next){
+//     if(req.isAuthenticated()){
+//         return next();
+//     }
 
-    res.redirect('/login');
-}
+//     res.redirect('/login');
+// }
 
-function checkNotAuthenticated(req,res,next){
-    if(req.isAuthenticated()){
-       return res.redirect('/')
-    }
-    next();
-}
+// function checkNotAuthenticated(req,res,next){
+//     if(req.isAuthenticated()){
+//        return res.redirect('/')
+//     }
+//     next();
+// }
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
